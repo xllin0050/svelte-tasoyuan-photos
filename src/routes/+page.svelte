3cc906b25e0b535ece06ed1db7f4e29690b8beaf
Carl Lin <xllin@tanzi>
2023-01-20 16:13:27 +0800

    feat: slide

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
-	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
 	import '@splidejs/svelte-splide/css';
+	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+	import { onMount } from 'svelte';
+
+	let main: Splide;
+	let thumbs: SplideSlide;
+	let screenWidth: number;
 
 	const demoPhotos: string[] = [
 		'IMG_0506',
@@ -14,14 +19,65 @@
 		'IMG_0516',
 		'IMG_0517'
 	];
+
+	const thumbsOptions = {
+		type: 'slide',
+		rewind: true,
+		gap: '0.5rem',
+		pagination: false,
+		isNavigation: true,
+		fixedWidth: '10%',
+		fixedHeight: 70,
+		cover: true,
+		drag: true,
+		updateOnMove: true,
+
+		breakpoints: {
+			640: {
+				destroy: true
+			}
+		}
+	};
+
+	onMount(() => {
+		if (main && thumbs) {
+			main.sync(thumbs.splide);
+		}
+	});
 </script>
 
+<svelte:window bind:innerWidth={screenWidth} />
 <section class="h-screen w-full bg-slate-400">
 	<div>
 		<h1>Hsu Tsao Yuan</h1>
 		<h2>許造元</h2>
+		{screenWidth}
 	</div>
-	<Splide aria-label="My Favorite Images">
+	<div class="max-w-[1575px] mx-auto">
+		<Splide bind:this={thumbs} options={thumbsOptions}>
+			{#each demoPhotos as url}
+				<SplideSlide>
+					<img src={`src/demo/${url}.jpg`} alt="demo" />
+				</SplideSlide>
+			{/each}
+		</Splide>
+	</div>
+	<Splide
+		bind:this={main}
+		aria-label="My Favorite Images"
+		options={{
+			width: '1575px',
+			type: 'fade',
+			arrows: false,
+			rewind: true,
+			mediaQuery: 'min',
+			breakpoints: {
+				1280: { height: '80vh' }
+			},
+			pagination: false
+		}}
+		class="mx-auto"
+	>
 		{#each demoPhotos as url}
 			<SplideSlide>
 				<img src={`src/demo/${url}.jpg`} alt="demo" />
